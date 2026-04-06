873edee87490f5e350cb17f945d66b7cc6362fe4
Paulo Henrique de Andrade <paulohenrique.andradefilho@gmail.com>
2026-04-06 20:20:23 -0300

    feat: initialize spring boot messaging consumer project

---
+++ backend-v1/src/main/java/br/com/fatec/messaging/MessagingConsumerApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.messaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingConsumerApplication.class, args);
+	}
+
+}
---
+++ backend-v1/src/test/java/br/com/fatec/messaging/MessagingConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
